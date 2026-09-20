0cf598999d928a21c2c0638ba1a5d4eedd3d6edd
talasiga <talasingatonga987@gmail.com>
2026-09-21 07:29:39 +1000

    Complete W07 Exercise Tracking

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,39 @@
+using System;
+
+public abstract class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public DateTime GetDate()
+    {
+        return _date;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    public abstract double GetDistance();
+
+    public abstract double GetSpeed();
+
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} {GetActivityName()} ({_minutes} min) - " +
+               $"Distance {GetDistance():0.0} miles, " +
+               $"Speed {GetSpeed():0.0} mph, " +
+               $"Pace: {GetPace():0.0} min per mile";
+    }
+
+    protected abstract string GetActivityName();
+}
